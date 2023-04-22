9e564f85df80b7ffc545c303589d9d7dde339374
Aren Dolbakian <aren.dolbakian@gmail.com>
2023-04-22 14:40:32 -0400

    Create Modal and Button components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import { Nunito } from 'next/font/google';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
+import Modal from './components/modal/Modal';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={font.className}>
 				<ClientOnly>
+				<Modal isOpen title={'Hello Wife'}/>
 				<Navbar />
 				</ClientOnly>
 				{children}
